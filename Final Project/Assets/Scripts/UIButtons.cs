3a0f69d66cfe21390ea01a0ef91c0d3da1e32acc
Marten2001 <martenalango@gmail.com>
2022-07-18 17:30:03 +0100

    added more scenes

---
+++ Final Project/Assets/Scripts/UIButtons.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void ControllsMenu()
+    {
+        SceneManager.LoadScene("ControllsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
